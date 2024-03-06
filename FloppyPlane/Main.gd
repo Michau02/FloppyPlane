d694a862874668bf362f38bde3414d7b1a02a5df
Michau02 <106237939+Michau02@users.noreply.github.com>
2024-03-06 23:45:48 +0100

    Add files via upload

---
+++ FloppyPlane/Main.gd
@@ -0,0 +1,98 @@
+extends Node2D
+
+@export var skyscrapers_scene : PackedScene
+
+var game_running = false
+var game_over = false
+
+var scroll_ground
+var scroll_background
+var score
+const scroll_background_speed = 1
+const scroll_ground_speed = 4
+var screen_size
+var ground_height
+var skyscrapers : Array
+const skyscraper_delay = 100
+const skyscraper_range = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_window().size
+	ground_height = $Ground.get_node("Sprite2D").texture.get_height()
+	newGame()
+	
+func newGame():
+	game_over = false
+	game_running = false
+	skyscrapers.clear()
+	generate_skyscrapers()
+	score = 0
+	scroll_ground = 0
+	scroll_background = 0
+	$Plane.reset()
+	
+func _input(event):
+	if !game_over:
+		if event is InputEventMouseButton:
+			if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+				if !game_running:
+					start_game()
+				else:
+					if $Plane.flying:
+						$Plane.flap()
+						check_top()
+
+func start_game():
+	game_running = true
+	$Plane.flying = true
+	$Plane.flap()
+	$SkycraperTimer.start()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if game_running:
+		scroll_ground += scroll_ground_speed
+		scroll_background += scroll_background_speed
+		if scroll_ground >= screen_size.x:
+			scroll_ground = 0
+		if scroll_background >= 4.5*screen_size.x:
+			scroll_background = 0
+			
+		$Ground.position.x = -scroll_ground	
+		$Background.position.x = -scroll_background
+		for skyscaper in skyscrapers:
+			skyscaper.position.x -= scroll_ground_speed
+		
+
+
+func _on_skycraper_timer_timeout():
+	generate_skyscrapers()
+	
+func generate_skyscrapers():
+	var skyscraper = skyscrapers_scene.instantiate()
+	skyscraper.position.x = screen_size.x + skyscraper_delay
+	skyscraper.position.y = (screen_size.y - ground_height) / 2 + randi_range(-skyscraper_range, skyscraper_range)
+	skyscraper.hit.connect(plane_hit)
+	add_child(skyscraper)
+	skyscrapers.append(skyscraper)
+	
+func check_top():
+	if $Plane.position.y < 0:
+		$Plane.falling = true	
+		stop_game()
+		
+func stop_game():
+	$SkycraperTimer.stop()
+	$Plane.flying = false
+	game_running = false
+	game_over = true		
+
+func plane_hit():
+	$Plane.falling = true
+	stop_game()
+
+
+func _on_ground_hit():
+	$Plane.falling = false
+	stop_game()
---
+++ FloppyPlane/plane.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+const max_velocity = 600
+const flap_speed = -500
+var flying = false
+var falling = false
+const starting_position = Vector2(100, 400) 
+
+func _ready():
+	reset()
+	
+func reset():
+	falling = false
+	flying = false
+	position = starting_position
+	set_rotation(0)
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	# Add the gravity.
+	if flying or falling:
+		velocity.y += gravity * delta
+		if velocity.y > max_velocity:
+			velocity.y = max_velocity
+		if flying:
+			set_rotation(deg_to_rad(velocity.y*0.05))	
+		elif falling:
+			set_rotation(PI/2)
+		move_and_collide(velocity*delta)
+			
+func flap():
+	velocity.y = flap_speed
